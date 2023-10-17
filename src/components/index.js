4f163c3b1ec64f1eb5185c4c0c0229415656d89a
Lucky <luckykashyap3430@gmail.com>
2023-10-17 17:23:00 +0530

    solved issue while scrolling to top

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,7 @@
+import { ScrollToTop } from "./ScrollToTop";
+
 export { Header } from "./Header";
 export { Footer } from "./Footer";
 export {Button} from './Button';
 export { MovieCard } from "./MovieCard";
+export {ScrollToTop} from './ScrollToTop';
--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import { MovieList, MovieDetail, Search, PageNotFound } from "../pages";
 
+
 export const AllRoutes = () => {
   return (
     <div className="dark:bg-darkbg">
       
         <Routes>
+         
             <Route path="" element={<MovieList apiPath="movie/now_playing" />} />
             <Route path="movie/:id" element={<MovieDetail />} />
             <Route path="movies/popular" element={<MovieList apiPath="movie/popular" />} />

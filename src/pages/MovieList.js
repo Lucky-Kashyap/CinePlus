98555072fc9083c3de2c11257fe54184c9818e23
Lucky <luckykashyap3430@gmail.com>
2023-10-17 16:17:26 +0530

    tailwind css customization

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -22,7 +22,7 @@ export const MovieList = ({apiPath}) => {
   return (
     <main>
       <section className="max-w-7xl mx-auto py-7">
-          <div className="flex justify-start flex-wrap">
+          <div className="flex justify-start flex-wrap other:justify-evenly">
             {/* <MovieCard/> */}
             {
               movies.map(movie=>(
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    darkMode:'class',
-    theme: {
-      extend: {},
+  content: ["./src/**/*.{html,js}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      minHeight: {
+        "90vh": "",
+      },
+      screens: {
+        other: { min: "340px", max: "1200px" },
+      },
+      colors: {
+        darkbg: "#1E293B",
+        green: {
+          850: "#047857",
+        },
+      },
     },
-    plugins: [],
-}
+  },
+  plugins: [],
+};

3e53593e982f9423024b06eeb45a9913273babb9
Lucky <luckykashyap3430@gmail.com>
2023-10-17 16:32:00 +0530

    add back arrow in button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,9 @@
 export const Button = ({children}) => {
     return (
-      <button className="w-64 text-xl text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 rounded-lg px-5 py-2.5 mr-2 mb-2 font-medium">{children}</button>
+      <button className="flex justify-evenly w-64 text-xl text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 rounded-lg px-5 py-2.5 mr-2 mb-2 font-medium"><svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5H1m0 0 4 4M1 5l4-4"/>
+  </svg>
+  {children}</button>
     )
   }
   
